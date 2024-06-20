b22d7323d5d57d18835612ab3a34ffd6197b13d2
Liliana Barbosa Alencar <146894518+LianaBarbosa@users.noreply.github.com>
2024-06-20 16:48:53 -0300

    Add files via upload

---
+++ OperadorExemplo6.java
@@ -0,0 +1,18 @@
+package turma_D; 
+
+public class OperadorExemplo6 {
+	public static void main(String[] args) {
+				String nome = "Tatá";
+				int idade = 10;
+				double nota = 4.5;
+				
+				
+				System.out.println(nome+" "+idade+" "+ nota);
+				System.out.printf("Seu nome é %S, sua idade é %d e sua nota foi %.3f " ,nome ,idade, nota);
+			
+				
+				
+		
+	}
+
+}
---
+++ OperadoresExemplo3.java
@@ -0,0 +1,11 @@
+package turma_D;
+
+public class OperadoresExemplo3 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		int c = 7;
+		System.out.println((a+b) / c);
+	}
+
+}
